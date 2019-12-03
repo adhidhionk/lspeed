6977af62f2f862c9efb288e254abc44af8612b56
Paget96 <mpaget96@gmail.com>
2019-12-03 12:26:34 +0100

    Removed useless logs

--- common/service.sh
+++ common/service.sh
@@ -1,16 +1,6 @@
 #!/system/bin/sh
 
-#PATHS
-LSPEED=/data/lspeed
-
-LOG_DIR=$LSPEED/logs
-LOG=$LOG_DIR/main_log.log
-
-echo "[$(date +"%H:%M:%S:%3N %d-%m-%Y")] Some loging started" | tee -a $LOG
-
 chmod 777 /system/etc/lspeed/lspeed.sh
 /system/etc/lspeed/lspeed.sh &
-
-echo "[$(date +"%H:%M:%S:%3N %d-%m-%Y")] Some loging ended" | tee -a $LOG
 
 exit 0
